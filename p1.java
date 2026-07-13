49851131d35e52cf80e1d0be1aefe5c620009698
ajitshinde0701 <shindeajit9590@gmail.com>
2026-07-13 11:40:01 +0530

    day 1st code..

---
+++ p1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Demo{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int temp =n;
+        int original = n;
+        int count =0;
+        int sum= 0;
+
+        while(temp>0){
+            count++;
+            temp/=10;
+        }
+
+        while(n>0){
+            int rem = n%10;
+
+            sum += Math.pow(rem,count);
+            n/=10;  
+        }
+
+        if(sum == original ){
+            System.out.println(original + " is an Armstrong number");
+        }
+        else{
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
+}
