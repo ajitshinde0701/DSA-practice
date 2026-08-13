4ce488356a125627a0a43e0a3b22fb88a2e5acb4
ajitshinde0701 <shindeajit9590@gmail.com>
2026-08-13 20:05:01 +0530

    code 10 added

---
+++ p10.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class Demo{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the starting integer : ");
+        int start= sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("Enter the Ending integer : ");
+
+        int end = sc.nextInt();
+
+        for(int i =start; i<=end ; i++){
+            if(i%2!=0){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
